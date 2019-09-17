853e7978b8aa12884724a565e74e4656f9406924
Kori SEFEANE <ksefeane@c5r8s9.wethinkcode.co.za>
2019-09-17 18:37:44 +0200

    added getName and greeting

--- setup.sh
+++ setup.sh
@@ -3,17 +3,45 @@
 clear
 echo "setup V0.1..\n"
 
-name="kori"
-user="N/A"
+name=""
+user=""
 date="NULL"
 clock="NULL"
-echo "welcome $name..\n"
 
-cat << end
-how are you today
-(genki desu ka?)
-end
-echo $cat
+getName()
+{
+	local name=""
+	local namefile="./name.txt"
+	if [ -e "$namefile" ]; then
+		cat $namefile
+	else
+		read -p "what's your name? " name
+		echo $name >> $namefile
+		cat $namefile
+	fi
+	return
+}
+greeting()
+{
+	local namefile="./name.txt"
+	local name=""
+	if [ -e "$namefile" ]; then
+		name=$(getName)
+		echo "welcome back $name.."
+	else
+		name=$(getName)
+		echo "hello $name\n"
+	fi
+	return
+}
+greeting
+#echo "\nwelcome $name..\n"
+
+#cat << end
+#how are you today
+#(genki desu ka?)
+#end
+#echo $cat
 
 getDate()
 {
@@ -25,10 +53,29 @@ getTime()
 	date "+%H:%M:%S"
 	return
 }
+getUser()
+{
+	local user=""
+	local userfile="./user.txt"
+	if [ -e "$userfile" ]; then
+		cat $userfile
+	else
+		read -p "please enter your username -> " user
+		echo $user >> $userfile
+		cat $userfile
+	fi
+	return
+}
 date=$(getDate)
 clock=$(getTime)
-echo "today is $date.."
+user=$(getUser)
+echo "\ntoday is $date.."
 echo "time: $clock..\n"
 
-read -p "kimi no namae wa? " user
-echo "\nhello $user"
+#if [ -z "$user" ]; then
+#	read -p "please enter your username -> " user
+#fi
+
+#read -p "kimi no namae wa? " user
+
+#echo "hello $user"
